ccb34b4cd23c72e2cc35dbc3f585ebafe961b07f
Darshansha <darshansha@gmail.com>
2025-08-29 14:11:54 +0100

    commit 1

---
+++ main1.py
@@ -0,0 +1,8 @@
+import math
+
+print("Calculate the circumference of a circle using the program below:")
+pi = math.pi
+r = int(input("Enter Radius of Circle: "))
+
+c = 2 * pi * r
+print("The circumference of your desired circle is: ", c)
